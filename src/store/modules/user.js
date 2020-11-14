5a1e0d9252dfa7476647001dac3569b99a318224
chinchin <chinchin@macOS.local>
2020-11-14 23:44:44 +0800

    .

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -7,20 +7,7 @@ export default {
     info: {
       name: '还未登录',
       avatar: 'https://newsirme-fresh.oss-cn-shenzhen.aliyuncs.com/backend/spu/20201103143638.png',
-      like_count: 0,
-      collect_count: 0,
-      comment_count: 0,
-      trace_count: 0,
-      coupon: 0,
-      balance: '0.00',
-      is_soldier: 0,
-      uid: '',
-      score: 0,
-      tel: '',
-      sex: 0,
-      birthday:''
     },
-    inviteCode: ''
   },
   mutations: {
     setToken(state, token) {
@@ -29,9 +16,6 @@ export default {
     setInfo(state, info) {
       state.info = info
     },
-    setInviteCode(state, code) {
-      state.inviteCode = code
-    }
   },
   actions:{
     updateInfo({commit}) {

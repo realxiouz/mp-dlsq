0ce3776a4099b161dbee599616d280118b6273ec
QinBo <xiouz@163.com>
2020-11-18 18:11:12 +0800

    .

--- src/common/js/const.js
+++ src/common/js/const.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://test.shop.81hbz.com/v1'
+export const BASE_URL = 'https://xzj.csywlkj.com/api/shop'
 
 export const AMAP_KEY = 'f3be860b7c9cb4d7bd69a88eb2e4ee96'
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,7 +4,23 @@
 
 <script>
 export default {
+  onLoad() {
+    this.$post('/index/template', {
+      type: 'banner'
+    })
+      .then(r => {
 
+      })
+    
+    wx.login({
+      success: ({code}) => {
+        console.log(code)
+        this.$post('/user/getWxMiniProgramSessionKey', {code})
+          .then(r => {
+          })
+      }
+    })
+  }
 }
 </script>
 
